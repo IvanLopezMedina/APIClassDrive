9093fcac44e46cc2b05ffb1fe5c59d6f6fd6ec17
Pau Teruel <93.pau.teruel@gmail.com>
2019-03-22 21:15:44 +0100

    Cors should have priority on time execution

--- app.js
+++ app.js
@@ -7,16 +7,8 @@ const cors = require('cors')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors())
 app.use('/api', users)
 app.use('/api', country)
-
-app.use(cors())
-
-app.all('/api/*', function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Headers', 'Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With')
-    res.header('Access-Control-Allow-Methods", "GET, PUT, POST')
-    return next()
-})
 
 module.exports = app
