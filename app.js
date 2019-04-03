7bf975a5b8779d0d3c3248f95bb3232a8a3ebc9a
Iván López Medina <ivan.lopez.medina.ilm@gmail.com>
2019-04-03 13:49:19 +0200

    app to master

--- app.js
+++ app.js
@@ -2,24 +2,15 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const users = require('./routes/users')
+const groups = require('./routes/groups')
 const forums = require('./routes/forums')
-const country = require('./routes/countries')
 const cors = require('cors')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use('/api', users)
-app.use('/api', forums)
-
-app.use('/api', country)
-
 app.use(cors())
-
-app.all('/api/*', function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Headers', 'Cache-Control, Pragma, Origin, Authorization, Content-Type, X-Requested-With')
-    res.header('Access-Control-Allow-Methods", "GET, PUT, POST')
-    return next()
-})
+app.use('/api', users)
+app.use('/api', groups)
+app.use('/api', forums)
 
 module.exports = app
