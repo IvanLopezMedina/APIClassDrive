371b38b8244168e4dfb07e152861343adfc437e7
Marta <marta.aguileraso@e-campus.uab.cat>
2019-04-08 00:28:29 +0200

    addFile v1
    Function addFile version 1

--- controllers/files.js
+++ controllers/files.js
@@ -7,3 +7,15 @@ const getFiles = (req, res) => {
 module.exports = {
     getFiles
 }
+
+const addFile = (req, res) => {
+    let file = new File()
+
+    file.name = req.body.name
+
+
+    file.save((err) => {
+        if (err) return res.status(409).send({ msg: `Error al subir archivo: ${err}` })
+        return res.status(200).send({ msg: `File added successfuly` })    
+    })
+}
