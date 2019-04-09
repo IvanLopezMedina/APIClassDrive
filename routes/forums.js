bf09948ffd9ba392f13de175854e268505f30ab0
Oscar <o.lopez.mundo@gmail.com>
2019-04-09 13:34:14 +0200

    Added deleteForumElement and route
    Still testing to do

--- routes/forums.js
+++ routes/forums.js
@@ -7,6 +7,7 @@ router.get('/posts/:forumId', forumsCtrl.getPosts)
 router.get('/post/:postId', forumsCtrl.getPost)
 router.put('/addPost/:forumId', forumsCtrl.addPost)
 router.put('/addAnswer/:forumId', forumsCtrl.addAnswer)
+router.delete('/deleteForumElement/:forumId', forumsCtrl.deleteForumElement)
 
 // Adding auth to the method, we ensure that the forum has a valid token
 // The token is send in the request body, with the tag: Authorization
