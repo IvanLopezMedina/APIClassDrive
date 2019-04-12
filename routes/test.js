a3a9488db426f638e5a8c60c81f7ad515c8c0240
Marc <marc.bernalo@e-campus.uab.cat>
2019-04-12 20:18:18 +0200

    Change Routes

--- routes/test.js
+++ routes/test.js
@@ -3,12 +3,12 @@ const express = require('express')
 const router = express.Router()
 const auth = require('../middlewares/auth')
 
-router.get('/allTests/:groupName', testCtrl.getAllTest)
+router.get('/alltests/:groupName', testCtrl.getAllTest)
 router.get('/test/:groupName', testCtrl.gestTest)
-router.put('/editTest/:testId', testCtrl.editTest)
-router.put('/deleteTest/:testId', testCtrl.deleteTest)
+router.put('/edittest/:testId', testCtrl.editTest)
+router.put('/deletetest/:testId', testCtrl.deleteTest)
 
-router.post('/addTest', testCtrl.addTest)
+router.post('/addtest', testCtrl.addTest)
 
 // Adding auth to the method, we ensure that the user has a valid token
 // The token is send in the request body, with the tag: Authorization
