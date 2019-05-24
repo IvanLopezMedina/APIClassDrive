5e25a1de9d0a2dc0a8a066d094e673bd9624669f
joancodinach <joan.codinach@e-campus.uab.cat>
2019-05-24 18:16:55 +0200

    Test acabat

--- test/files_unit.test.js
+++ test/files_unit.test.js
@@ -21,9 +21,10 @@ mongoose.connect(config.db, (err, res) => {
 })
 
 describe('API Tests', function () {
+    var file = { fileId: '5ce534b5f52c38050c1ee8a6', userId: '5ce5340af52c38050c1ee89f' }
     describe('## Function getFiles', function () {
         it('should get all the files', function () {
-            request(app).get('/api/getfiles/lis').end(function (err, res) {
+            request(app).get('/api/files/getfiles/lis').end(function (err, res) {
                 if (err) expect(res.statusCode).to.equal(404)
                 assert.strictEqual(res.statusCode, 200)
             })
@@ -31,15 +32,15 @@ describe('API Tests', function () {
     })
     describe('## Function getFiles', function () {
         it('should get an error because the name doesnt exist', function () {
-            request(app).get('/api/getfiles/groupnameinvented').end(function (err, res) {
+            request(app).get('/api/files/getfiles/groupnameinvented').end(function (err, res) {
                 if (err) expect(res.statusCode).to.equal(200)
-                assert.strictEqual(res.statusCode, 404)
+                assert.strictEqual(res.statusCode, 409)
             })
         })
     })
     describe('## Function getFile', function () {
         it('should get the file by the fileId', function () {
-            request(app).get('/api/getfile/5cb848ef2ecdb21e6463bf67').end(function (err, res) {
+            request(app).get('/api/files/getfile/5cb848ef2ecdb21e6463bf67').end(function (err, res) {
                 if (err) expect(res.statusCode).to.equal(404)
                 assert.strictEqual(res.statusCode, 200)
             })
@@ -47,23 +48,15 @@ describe('API Tests', function () {
     })
     describe('## Function getFile', function () {
         it('should get an error because the id doesnt exist', function () {
-            request(app).get('/api/getfile/noexisteix').end(function (err, res) {
+            request(app).get('/api/files/getfile/noexisteix').end(function (err, res) {
                 if (err) expect(res.statusCode).to.equal(200)
-                assert.strictEqual(res.statusCode, 404)
-            })
-        })
-    })
-    describe('## Function addFile', function () {
-        it('should add a file', function () {
-            request(app).post('/api/addFile/lis').end(function (err, res) {
-                if (err) expect(res.statusCode).to.equal(404)
-                assert.strictEqual(res.statusCode, 200)
+                assert.strictEqual(res.statusCode, 409)
             })
         })
     })
     describe('## Function deleteFile', function () {
         it('should delete a file', function () {
-            request(app).put('/api/deleteFile/lis').end(function (err, res) {
+            request(app).put('/api/files/deleteFile/lolipop').send(file).end(function (err, res) {
                 if (err) expect(res.statusCode).to.equal(404)
                 assert.strictEqual(res.statusCode, 200)
             })
